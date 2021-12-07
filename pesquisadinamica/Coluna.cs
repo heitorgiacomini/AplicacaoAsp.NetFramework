cbcea0b82490190b9628fbb1fc81c26d1a2e1485
Heitor Giacomini <heithor_thebest@hotmail.com>
2021-12-07 14:32:39 -0300

    Create Coluna.cs

---
+++ pesquisadinamica/Coluna.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidade.Comum
+{
+    public class Coluna
+    {
+        public String Nome { get; set; }
+        public String ApelidoNome{ get; set; }
+    }
+}
