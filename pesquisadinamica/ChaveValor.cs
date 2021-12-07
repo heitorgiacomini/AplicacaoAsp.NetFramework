af5363ecdd68a14fa4b1448fca8f03fcfb9e8757
Heitor Giacomini <heithor_thebest@hotmail.com>
2021-12-07 14:33:20 -0300

    Create ChaveValor.cs

---
+++ pesquisadinamica/ChaveValor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidade.Comum
+{
+    public class ChaveValor
+    {
+        public string Chave { get; set; }
+        public string Valor { get; set; }
+    }
+}
